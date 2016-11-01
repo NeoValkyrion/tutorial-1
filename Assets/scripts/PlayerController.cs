87b7d37da2f45bdf7b5e5180b5157952ea2bf8b9
Dan Neumann <dan@bestangle.com>
2016-11-01 10:05:07 -0700

    Create project repository for Roll A Ball Unity tutorial
    - Add first draft of Unity project gitignore
    - Finished parts 1 and 2 of tutorial

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update ()
+    {
+        // This method is called before rendering a frame
+        // Most game code goes here
+    }
+
+    void FixedUpdate()
+    {
+        // This method is called before any physics calculations
+        // Most physics code goes here
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
